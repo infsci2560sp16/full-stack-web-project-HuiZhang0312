b9782e652b121acee0c0e324dc51c792f3ac81cc
HuiZhang0312 <zhanghui312@outlook.com>
2016-03-31 13:10:25 -0400

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
+public class Test {
+
+  public static void main(String[] args) {
+
+    port(Integer.valueOf(System.getenv("PORT")));
+    staticFileLocation("/public");
+
+    get("/a", (req, res) -> "a");
+
+  }
+
+}
