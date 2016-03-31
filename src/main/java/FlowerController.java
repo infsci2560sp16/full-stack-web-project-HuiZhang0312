b7d0534bf1aecd44eb89dca3ce2ffd8bebd768b6
HuiZhang0312 <zhanghui312@outlook.com>
2016-03-31 15:55:16 -0400

    test

--- src/main/java/FlowerController.java
+++ src/main/java/FlowerController.java
@@ -2,12 +2,7 @@ import static spark.Spark.*;
 import spark.*;
 
 public class FlowerController {
-	public UserController(final FlowerService flowerService){
-		get("/flowers", new Route(){
-			@Override
-			public Object handle(Request request, Response response){
-				return  flowerService.getAllFlowers();
-			}
-		});
+	public FlowerController(final FlowerService flowerService) {
+    get("/flowers", (req, res) -> {flowerService.getAllFlowers()});
 	}
 }
