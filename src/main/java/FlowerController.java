ee5e1047928bafa6748e0af772eccb7e69b2bbf5
HuiZhang0312 <zhanghui312@outlook.com>
2016-03-31 15:29:00 -0400

    test

--- src/main/java/FlowerController.java
+++ src/main/java/FlowerController.java
@@ -2,8 +2,11 @@ package main;
 
 public class FlowerController {
 	public FlowerController(final FlowerService flowerService) {
-		
-		get("/flowers", (req, res) -> flowerService.getAllFlowers());
-	     
-	  }
+    get("/flowers", new Route() {
+      @Override
+      public Object handle(Request request, Response response) {
+        // process request
+        return flowerService.getAllFlowers();
+      }
+    });
 }
