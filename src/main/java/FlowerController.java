5f1b8b7442572766310c82d93792ad39f4160028
HuiZhang0312 <zhanghui312@outlook.com>
2016-03-31 18:57:39 -0400

    test

--- src/main/java/FlowerController.java
+++ src/main/java/FlowerController.java
@@ -11,7 +11,7 @@ public class FlowerController {
         // process request
         return flowerService.getAllFlowers();
       }
-    });
+    },json());
 
     // more routes
   }
---
+++ src/main/java/JsonUtil.java
@@ -0,0 +1,10 @@
+public class JsonUtil {
+
+  public static String toJson(Object object) {
+    return new Gson().toJson(object);
+  }
+
+  public static ResponseTransformer json() {
+    return JsonUtil::toJson;
+  }
+}
